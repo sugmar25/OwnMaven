c911d1ed177c74c6d47f2c29cf13125df4499913
Sugumar <sugumar.sampath@usbank.com>
2020-05-05 04:03:42 +0530

    add testNG

--- src/test/java/testcaseExecution/Lanchchrome.java
+++ src/test/java/testcaseExecution/Lanchchrome.java
@@ -2,9 +2,12 @@ package testcaseExecution;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Lanchchrome {
-	public static void main(String [] args) {
+	@Test
+	public void initPageClasses()
+	 {
 		System.setProperty("webdriver.chrome.driver", "/Users/786516/Downloads/ui_automation-Team_OmniRPS_KPC_Features/src/test/resources/Drivers/chromedriver");
 	    WebDriver driver =new ChromeDriver();
 	    driver.get("https://www.google.com/");
