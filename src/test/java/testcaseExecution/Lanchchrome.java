acc670c4e3b76981f10756ea2404d45cb3a3f382
Sugumar <sugumar.sampath@usbank.com>
2020-05-05 03:59:52 +0530

    addchrome

---
+++ src/test/java/testcaseExecution/Lanchchrome.java
@@ -0,0 +1,16 @@
+package testcaseExecution;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lanchchrome {
+	public static void main(String [] args) {
+		System.setProperty("webdriver.chrome.driver", "/Users/786516/Downloads/ui_automation-Team_OmniRPS_KPC_Features/src/test/resources/Drivers/chromedriver");
+	    WebDriver driver =new ChromeDriver();
+	    driver.get("https://www.google.com/");
+	    System.out.println(driver.getTitle());
+	    driver.close();
+	
+	}
+
+}
